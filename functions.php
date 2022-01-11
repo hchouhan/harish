a76e59fb028fd8851058c08fadfae372bd0d214c
Harish Chouhan <me@harishchouhan.com>
2022-01-12 04:34:14 +0530

    More stable now

--- functions.php
+++ functions.php
@@ -7,3 +7,12 @@
  * @license GNU General Public License v3 or later
  * @link    https://harishchouhan.com/
  */
+
+// Enqueue style sheet on frontend.
+
+function harish_enqueue_style_sheet() {
+
+	wp_enqueue_style( 'harish-styles', get_stylesheet_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
+
+}
+add_action( 'wp_enqueue_scripts', 'harish_enqueue_style_sheet' );
